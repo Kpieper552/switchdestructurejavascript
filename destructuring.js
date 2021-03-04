6e2c4dea3d41baa95a0e511bd15d5c7041792aba
Kpieper552 <k.pieper@novi-education.nl>
2021-03-04 12:22:45 +0100

    destructure

--- destructuring.js
+++ destructuring.js
@@ -6,6 +6,11 @@ const product = {
   screenType: 'LED',
 }
 
+const { price, refreshRate } = product;
+console.log("price " + price);
+console.log("refreshRate " + refreshRate);
+
+
 // Opdracht 2: destructure type en brand en log ze in de console
 
 const productInformation = {
@@ -17,6 +22,10 @@ const productInformation = {
   screenQuality: 'Ultra HD/4K',
   smartTv: true,
 }
+
+const { type, brand } = productInformation.general;
+console.log("type " + type);
+console.log("brand " + brand);
 
 // Opdracht 3: destructure wifi en bluetooth en log ze in de console
 
@@ -38,13 +47,20 @@ const tvOptions = {
   },
 };
 
+const { wifi, bluetooth } = tvOptions.options.connectivity.popular;
+console.log("wifi " + wifi);
+console.log("bluetooth " + bluetooth);
+
+
 // Opdracht 4: destructure beide waardes uit deze array en log ze in de console
 
-const connectivity = ['wifi', 'bluetooth'];
+const connectivity = ['wifi ' + wifi + 'bluetooth  ' + bluetooth];
+console.log(connectivity);
 
 // Opdracht 5: destructure de eerste en derde waarde uit deze array en log ze in de console
 
 const tvSizes = [41, 43, 46, 56, 65];
+console.log(tvSizes[0],tvSizes[2]);
 
 // opdracht 6: destructure name en adress uit de return value van deze functie en log ze in de console
 
@@ -54,9 +70,15 @@ function getCompanyDetails() {
     adress: 'Zonnebaan 9, Utrecht',
   }
 }
+const details = getCompanyDetails();
+console.log(details);
+
 
 // opdracht 7: destructure beide zinnetjes uit de return value van deze functie en log ze in de console. Let op: de functie verwacht argumenten bij het aanroepen!
 
 function getDetails(name, age) {
   return [`Your name is ${name}`, `You are ${age} years old`];
 }
+
+detailsPerson = getDetails();
+console.log(detailsPerson);
